89dc752cbd762eb41e7cd9d388fbff58bce5d88a
sven <sven.song@qq.com>
2024-10-25 07:58:16 +0800

    P119 1:57

--- src/components/3_hooks/index.jsx
+++ src/components/3_hooks/index.jsx
@@ -1,57 +1,69 @@
 import React, { Component } from 'react'
 
-// export default class Demo extends Component {
+ class Demo extends Component {
 
-//   state = {
-//     count: 0
-//   }
-
-//   add = ()=> {
-//     this.setState({
-//       count: this.state.count + 1
-//     })
-//   }
-
-//   render() {
-//     return (
-//       <div>
-//         <h2>当前求和为:{this.state.count}</h2>
-//         <button onClick={this.add}>点我加1</button>
-//       </div>
-//     )
-//   }
-// }
-
-
-function Demo (){
-
-  const [count, setCount] = React.useState(0)
-  const [name, setName] = React.useState('Tom')
-
-  function add(){
-    console.log('0000000')
-    // setCount(count + 1)
-
-    setCount(count =>  count + 1)
+  state = {
+    count: 0
   }
 
+  componentDidMount(){
+    
+    setInterval(() => {
 
-  function changeName(){
-    setName('Jack')
+      this.setState({
+        count : this.state.count + 1
+      })
+    }, 500);
+
+
   }
 
-  return(
+  add = ()=> {
+    this.setState({
+      count: this.state.count + 1
+    })
+  }
 
+  render() {
+    return (
       <div>
-        <h2>当前求和为:{count}</h2>
-        <button onClick={add}>点我加1</button>
-
-        <h2>我的名字:{name}</h2>
-        <button onClick={changeName}>点我改名</button>
+        <h2>当前求和为:{this.state.count}</h2>
+        <button onClick={this.add}>点我加1</button>
       </div>
-
-  )
+    )
+  }
 }
+
+
+// function Demo (){
+
+//   const [count, setCount] = React.useState(0)
+//   const [name, setName] = React.useState('Tom')
+
+//   function add(){
+//     console.log('0000000')
+//     // setCount(count + 1)
+
+//     setCount(count =>  count + 1)
+//   }
+
+
+//   function changeName(){
+//     setName('Jack')
+//   }
+
+//   return(
+
+//       <div>
+//         <h2>当前求和为:{count}</h2>
+//         <button onClick={add}>点我加1</button>
+
+//         <h2>我的名字:{name}</h2>
+//         <button onClick={changeName}>点我改名</button>
+//       </div>
+
+//   )
+// }
 
 
 export default Demo
