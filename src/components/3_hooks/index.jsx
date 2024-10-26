a28c63268279d7cd8765275d2f180148251f56f7
sven <sven.song@qq.com>
2024-10-26 08:39:05 +0800

    P119 19:13

--- src/components/3_hooks/index.jsx
+++ src/components/3_hooks/index.jsx
@@ -1,69 +1,88 @@
 import React, { Component } from 'react'
+import ReactDom from 'react-dom'
 
- class Demo extends Component {
+//  class Demo extends Component {
 
-  state = {
-    count: 0
-  }
+//   state = {
+//     count: 0
+//   }
 
-  componentDidMount(){
+//   componentDidMount(){
     
-    setInterval(() => {
+//     this.timer = setInterval(() => {
 
-      this.setState({
-        count : this.state.count + 1
-      })
-    }, 500);
-
-
-  }
-
-  add = ()=> {
-    this.setState({
-      count: this.state.count + 1
-    })
-  }
-
-  render() {
-    return (
-      <div>
-        <h2>当前求和为:{this.state.count}</h2>
-        <button onClick={this.add}>点我加1</button>
-      </div>
-    )
-  }
-}
+//       this.setState({
+//         count : this.state.count + 1
+//       })
+//     }, 500);
 
 
-// function Demo (){
-
-//   const [count, setCount] = React.useState(0)
-//   const [name, setName] = React.useState('Tom')
-
-//   function add(){
-//     console.log('0000000')
-//     // setCount(count + 1)
-
-//     setCount(count =>  count + 1)
 //   }
 
-
-//   function changeName(){
-//     setName('Jack')
+//   componentWillUnmount(){
+//     clearInterval(this.timer)
 //   }
 
-//   return(
+//   remove =()=>{
+//     ReactDom.unmountComponentAtNode(document.getElementById('root'))
+//   }
 
+//   add = ()=> {
+//     this.setState({
+//       count: this.state.count + 1
+//     })
+//   }
+
+//   render() {
+//     return (
 //       <div>
-//         <h2>当前求和为:{count}</h2>
-//         <button onClick={add}>点我加1</button>
-
-//         <h2>我的名字:{name}</h2>
-//         <button onClick={changeName}>点我改名</button>
+//         <h2>当前求和为:{this.state.count}</h2>
+//         <button onClick={this.add}>点我加1</button>
+//         <button onClick={this.remove}>点我卸载</button>
 //       </div>
-
-//   )
+//     )
+//   }
 // }
+
+// 函数里面没有this
+function Demo (){
+
+  const [count, setCount] = React.useState(0)
+
+
+  React.useEffect(()=>{
+    console.log('@')
+    let timer = setInterval(() => {
+      setCount(count => count + 1)
+    }, 1000);
+    const iiii = 99
+    return ()=> { // 相当于willUnmount
+      console.log('####',iiii)
+      clearInterval(timer)
+    }
+  },[]) //空数组表示谁也不检测; 数组中写谁,表示检测谁 ; 相当于
+
+  function add(){
+
+    setCount(count =>  count + 1)
+  }
+
+  function remove(){
+    ReactDom.unmountComponentAtNode(document.getElementById('root'))
+  }
+
+  return(
+
+      <div>
+        <h2>当前求和为:{count}</h2>
+        <button onClick={add}>???点我加1</button>
+        <button onClick={remove}>点我卸载</button>
+
+        
+      </div>
+
+  )
+}
 
 
 export default Demo
