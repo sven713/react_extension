934ab5a693a451ead1f223f1c5b1a135f6b8df16
sven <sven.song@qq.com>
2024-10-26 14:27:13 +0800

    P121 Fragment,去掉必须的容器div

--- src/App.js
+++ src/App.js
@@ -1,14 +1,14 @@
 import React, { Component } from 'react'
 
-import Demo from './components/3_hooks'
+import Demo from './components/4_Fragment'
 
 export default class App extends Component {
   render() {
     return (
-      <div>
+      <>
 
         <Demo></Demo>
-      </div>
+      </>
     )
   }
 }
