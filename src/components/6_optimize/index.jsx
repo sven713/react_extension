67417cadaa05812bc31692f116d286248d49a6c9
sven <sven.song@qq.com>
2024-10-27 08:36:19 +0800

    P123 OK setState要用新对象，否则页面不刷新

--- src/components/6_optimize/index.jsx
+++ src/components/6_optimize/index.jsx
@@ -4,13 +4,28 @@ import './index.css'
 export default class Parent extends PureComponent {
 
   state = {
-    carName:'BYD汉'
+    carName:'BYD汉',
+    stus:['小李','校长','小王']
   }
   changeCar =()=> {
     this.setState({
       carName:'特斯拉'
     })
     // this.setState({})
+  }
+
+  addStud = ()=> {
+    const {stus} = this.state
+    // stus.push('lisi')
+    // this.setState({
+    //   stus
+    // })
+    // console.log('lll---',stus,'state---',this.state.stus)
+
+    this.setState({
+      stus:[...stus, 'lisi']
+    })
+    //!!!! setState, 用新对象,别在原始对象上push, unshift
   }
 
   // shouldComponentUpdate (event){
@@ -25,8 +40,10 @@ export default class Parent extends PureComponent {
     return (
       <div className='parent'>
         <h3>我是parent组件</h3>
+        {this.state.stus}<br></br>
         <span>我车的名字:{carName}</span><br></br>
         <button onClick={this.changeCar}>点我换车</button>
+        <button onClick={this.addStud}>点我添加学生</button>
         <Child carName={carName}/>
       </div>
     )
