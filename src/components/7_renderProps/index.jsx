6feeffb64972a278851bb17159dd203c5b24b54b
sven <sven.song@qq.com>
2024-10-27 16:40:55 +0800

    P124 render Props=== Vue slot插槽技术，容器组件可以随意插入其他组件，并传参

--- src/components/7_renderProps/index.jsx
+++ src/components/7_renderProps/index.jsx
@@ -1,24 +1,31 @@
 import React, { Component } from 'react'
 import './index.css'
+import C from '../1_setState'
 
 export default class Parent extends Component {
   render() {
     return (
       <div className='parent'>
         <h3>Parent</h3>
-        <A>hello</A>
+        <A render={(name)=> (<C/>)}>
+
+        </A>
       </div>
     )
   }
 }
 
 class A extends Component {
+  state={
+    name:'tom1'
+  }
   render() {
     console.log('aa-',this.props)
+    const {name} = this.state
     return (
       <div className='a'>
         <h3>我是A组件</h3>
-        {this.props.children}
+        {this.props.render(name)}
         {/* <B></B> */}
       </div>
     )
@@ -29,7 +36,7 @@ class B extends Component {
   render() {
     return (
       <div className='b'>
-        <h3>我是B组件</h3>
+        <h3>我是B组件,{this.props.name}</h3>
       </div>
     )
   }
